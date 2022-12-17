22202512bf879ce9705a50cc6e3f5c485698bbd2
Riku0413 <riku.k.18.soccer@gmail.com>
2022-12-17 14:55:10 +0900

    made my_???? functions in preparation for the assignment 3.

--- simulation/bouncing.c
+++ simulation/bouncing.c
@@ -9,16 +9,16 @@ int main(int argc, char **argv) {
         {
             .width  = 75,
             .height = 40,
-            .G = 1.0,
-            .dt = 1.0,
-            .cor = 0.8
+            .G = 1.0, // 重力G
+            .dt = 1.0, // 何秒刻みで何秒刻みで時間が進むか
+            .cor = 0.8 // 反発係数
         };
     
     size_t objnum = 2;
     Object objects[objnum];
     
     // objects[1] は巨大な物体を画面外に... 地球のようなものを想定
-    objects[0] = (Object){ .m = 60.0, .y = -19.9, .vy = 2.0};
+    objects[0] = (Object){ .m = 60.0, .y = -19.9, .vy = 0.0}; // 質量、初期位置、初期速度（yは下向き正）
     objects[1] = (Object){ .m = 100000.0, .y =  1000.0, .vy = 0.0};
     
     // シミュレーション. ループは整数で回しつつ、実数時間も更新する
@@ -26,12 +26,12 @@ int main(int argc, char **argv) {
     double t = 0;
     for (size_t i = 0 ; t <= stop_time ; i++){
         t = i * cond.dt;
-        update_velocities(objects, objnum, cond);
-        update_positions(objects, objnum, cond);
-        bounce(objects, objnum, cond); // 壁があると仮定した場合に壁を跨いでいたら反射させる
+        my_update_velocities(objects, objnum, cond);
+        my_update_positions(objects, objnum, cond);
+        my_bounce(objects, objnum, cond); // 壁があると仮定した場合に壁を跨いでいたら反射させる
         
         // 表示の座標系は width/2, height/2 のピクセル位置が原点となるようにする
-        plot_objects(objects, objnum, t, cond);
+        my_plot_objects(objects, objnum, t, cond);
         
         usleep(200 * 1000); // 200 x 1000us = 200 ms ずつ停止
         printf("\e[%dA", cond.height+3);// 壁とパラメータ表示分で3行
--- simulation/physics2.h
+++ simulation/physics2.h
@@ -11,8 +11,8 @@ typedef struct condition {
 // 個々の物体を表す構造体
 typedef struct object {
     double m;
-    double y;
-    double prev_y; // 壁からの反発に使用
+    double y; // 最新時刻の物理量？
+    double prev_y; // 壁からの反発に使用 // 一個前の時刻の物理量？
     double vy;
     double prev_vy;
 } Object;
@@ -26,4 +26,86 @@ void bounce(Object objs[], const size_t numobj, const Condition cond);
 
 // To do: 以下に上記と同じ引数で実行される my_* を作成
 // 実装できたらmain関数で上記と入れ替えていく
-// my_plot_objects(), my_update_velocities(), my_update_positions(), my_bounce の4つ 
+// my_plot_objects(), my_update_velocities(), my_update_positions(), my_bounce の4つ
+
+void my_update_velocities(Object objs[], const size_t numobj, const Condition cond) {
+    for (int i = 0; i < numobj; i++) {
+        double a = 0;
+        for (int j = 0; j < numobj; j++) {
+            if (j == i) {
+                continue; // 自分からの引力はない
+            }
+            a += objs[j].m * (objs[j].y - objs[i].y) / pow(fabs(objs[j].y - objs[i].y), 3.0);
+        }
+        a *= cond.G;
+        objs[i].prev_vy = objs[i].vy;
+        objs[i].vy += a * cond.dt;
+    }
+}
+
+void my_update_positions(Object objs[], const size_t numobj, const Condition cond) {
+    for (int i = 0; i < numobj; i++) {
+        objs[i].prev_y = objs[i].y;
+        objs[i].y += objs[i].prev_vy * cond.dt;
+    }
+}
+
+void my_bounce(Object objs[], const size_t numobj, const Condition cond) {
+    double y_min = - cond.height/2;
+    double y_max = cond.height/2;
+    for (int i = 0; i < numobj; i++) {
+        if (objs[i].prev_y > y_min && objs[i].prev_y < y_max && objs[i].y > y_max) {
+            double dt1 = (y_max - objs[i].prev_y)/objs[i].prev_vy;
+            double dt2 = cond.dt - dt1;
+            objs[i].vy *= -cond.cor;
+            objs[i].y = y_max + objs[i].prev_vy * (-cond.cor) * dt2;
+        } else if (objs[i].prev_y > y_min && objs[i].prev_y < y_max && objs[i].y < y_min) {
+            double dt1 = (objs[i].prev_y - y_min)/fabs(objs[i].prev_vy);
+            double dt2 = cond.dt - dt1;
+            objs[i].vy *= -cond.cor;
+            objs[i].y = y_min + objs[i].prev_vy * (-cond.cor) * dt2;
+        }
+    }
+}
+
+void my_plot_objects(Object objs[], const size_t numobj, const double t, const Condition cond) {
+
+    printf("+");
+    for (int j = 0; j < cond.width; j++) {
+        printf("-");
+    }
+    printf("+\r\n"); // \r は要る？
+
+    for (int i = 0; i < cond.height; i++) {
+        printf("|");
+        for (int j = 0; j < cond.width; j++) {
+            printf(" ");
+            for (int k = 0; k < numobj; k++) {
+                if (cond.height/2 + round(objs[k].y) == i && j == (cond.width + 1)/2) {
+                    printf("\bo"); // 空白を一個消した上で物体をプロット
+                    break;
+                } else if (cond.height/2 + round(objs[k].y) == 40 && i == 39 && j == (cond.width + 1)/2) {
+                    printf("\bo"); // y = 40 のときに物体が消失しないように調整
+                    break;
+                }
+            }
+        }
+        printf("|\r\n");
+    }
+
+    printf("+");
+    for (int j = 0; j < cond.width; j++) {
+        printf("-");
+    }
+    printf("+\r\n");
+
+    printf("t = %lf, ", t);
+    for (int k = 0; k < numobj; k++) {
+        printf("objs[%d].y = %lf", k, objs[k].y);
+        if (k != numobj - 1) {
+            printf(", ");
+        }
+    }
+    printf("\r\n");
+
+}
